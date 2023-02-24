9d9b78aaf6506faf8e398d623c57bb0565d715e0
yuliussetyawan <yuliusetyawan@gmail.com>
2023-02-24 14:31:27 +0700

    Understanding Templating vs. Layouts

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-let items = ["Buy food", "Cook food", "Eat food"];
+const items = ["Buy food", "Cook food", "Eat food"];
+const workList = [];
 
 app.get("/", (req, res) => {
   let options = {
@@ -20,14 +21,26 @@ app.get("/", (req, res) => {
   let day = today.toLocaleDateString("id-ID", options);
 
   //must provide both variables in ejs render
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", (req, res) => {
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if (req.body.list === "Work List") {
+    workList.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
-app.listen(3000, () => console.log("server is up and running"));
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workList });
+});
 
+app.get("/about", (req,res) => {
+  res.render("about");
+})
+
+app.listen(3000, () => console.log("server is up and running"));
