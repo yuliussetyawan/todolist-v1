535b00e77662bc2686b92d4f6c2862670b819812
yuliussetyawan <yuliusetyawan@gmail.com>
2023-02-24 13:53:37 +0700

    Adding css

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 let items = ["Buy food", "Cook food", "Eat food"];
 
