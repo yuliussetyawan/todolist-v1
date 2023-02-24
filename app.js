d2ab36d9d773b720c30deb46a23dd7bcb800cbdc
yuliussetyawan <yuliusetyawan@gmail.com>
2023-02-24 13:21:10 +0700

    Passing Data from the Webpage to the Server

--- app.js
+++ app.js
@@ -2,38 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
+let items = ["Buy food", "Cook food", "Eat food"];
+
 app.get("/", (req, res) => {
-  let day;
-  const today = new Date().getDay();
-  switch (today) {
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    case 0:
-      day = "Sunday";
-      break;
-    default:
-      day = `Error: the current day is equal to ${today}`;
-  }
-  res.render("list", { kindOfDay: day });
+  let options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  let today = new Date();
+  let day = today.toLocaleDateString("id-ID", options);
+
+  //must provide both variables in ejs render
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, () => console.log("server is up and running"));
+
